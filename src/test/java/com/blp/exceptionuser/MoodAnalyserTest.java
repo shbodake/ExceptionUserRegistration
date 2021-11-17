f5d2b87f9b4d4e0bb2ee62d922d83f8866e8aca8
shbodake <sayalibodake28@gmail.com>
2021-11-17 16:07:03 +0530

    Added: Written mood analyser and test case validates the entry successfully or not

---
+++ src/test/java/com/blp/exceptionuser/MoodAnalyserTest.java
@@ -0,0 +1,22 @@
+package com.blp.exceptionuser;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MoodAnalyserTest {
+    MoodAnalyser moodAnalyser = new MoodAnalyser();
+
+    @Test
+    public void givenMessageWhenHappyReturnEntrySuccessful() {
+        String actualResult = moodAnalyser.analyseMood("User is Happy");
+        Assert.assertEquals("Entry Successful", actualResult);
+    }
+
+    @Test
+    public void givenEmail2WhenNotProperReturnEntryFailed() {
+        String actualResult = moodAnalyser.analyseMood("User is Sad");
+        Assert.assertEquals("Entry Failed", actualResult);
+    }
+}
